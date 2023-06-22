fce44d49dfc3e7fad753a6ca3de55f87b5d123cd
James <james.ch.pino@gmail.com>
2023-06-22 14:11:37 -0400

    Adding ternary model example, also updating creating run files

---
+++ binary_models/load_ternary.py
@@ -0,0 +1,33 @@
+from micom import load_pickle
+import pathlib
+import pandas as pd
+import binary_models.helpers as helpers
+
+_suffix = 'ternary'
+m_names = ['Rhodosporidium', 'Synechococcus', 'Azotobacter']
+f_names = ['Rhodo_Toru.xml', 'syn_elong.xml', 'azo_vine.xml']
+
+path = pathlib.Path(__file__).parent
+
+model_folder = path.joinpath(f'models_{_suffix}').__str__()
+_db_path = path.joinpath(f'db_{_suffix}').__str__()
+_mani_path = path.joinpath('manifest_files', f'man_{_suffix}.csv').__str__()
+
+
+def load_community_model(prep_files=False):
+    mani_exists = pathlib.Path(model_folder).joinpath('manifest.csv').exists()
+    if prep_files or not mani_exists:
+        manifest = helpers.prepare_files(
+            m_names, f_names, _mani_path, _db_path, model_folder, path
+        )
+    else:
+        manifest = pd.read_csv(
+            pathlib.Path(model_folder).joinpath('manifest.csv')
+        )
+    f_path = pathlib.Path(model_folder).joinpath('One.pickle').__str__()
+    community = load_pickle(f_path)
+    return community, manifest
+
+
+if __name__ == '__main__':
+    load_community_model(prep_files=True)
